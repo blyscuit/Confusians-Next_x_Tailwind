23c02beca29f927cb5461810384b29170fce43d1
Bliss <pisit.w@23perspective.com>
2020-04-17 12:44:12 +0700

    remove unuse imports

--- pages/blog/page/[page].js
+++ pages/blog/page/[page].js
@@ -1,8 +1,4 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import Post from '../../../components/blog/post'
-import { Helmet } from 'react-helmet';
-import Layout from '../../../components/MyLayout'
+
 import HomePage from '../../blog'
 
 export default HomePage
